4937fb39c3ebaf65c3eedd32c7176f4ecefaf1ed
siddharth189 <siddharthiiitg@gmail.com>
2023-01-29 14:10:37 +0530

    added initials

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,13 @@
+import { TbCurrencyRupee } from "react-icons/tb";
 function Card(props) {
   return (
     <div class="panel">
       <div class="pricing-plan">
         <img src={props.img} alt="product-image" class="pricing-img" />
         <h2 class="pricing-header">{props.name}</h2>
-        <span class="pricing-price">Rs{props.price}</span>
+        <span class="pricing-price">
+          <TbCurrencyRupee /> {props.price}
+        </span>
         <button className="shop-now">Add to Cart</button>
       </div>
     </div>
